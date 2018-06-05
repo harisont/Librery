ca0fc652b02eebc832b6c090c2d4949bd4b02552
Arianna Masciolini <uzkamascio@gmail.com>
2018-06-05 17:32:40 +0200

    Huge progress in querying the database.

--- LibreryApp/app/src/main/java/com/example/harisont/librery/SearchActivity.kt
+++ LibreryApp/app/src/main/java/com/example/harisont/librery/SearchActivity.kt
@@ -14,23 +14,37 @@ class SearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search)
 
         search_button.setOnClickListener {
-            advancedSearch(null, null, null, null)
+            advancedSearch(
+                    search_by_isbn.text.toString(),
+                    search_by_title.text.toString(),
+                    search_by_author.text.toString(),
+                    search_by_publisher.text.toString())
         }
     }
 
-    private fun advancedSearch(isbn: String?, intitle: String?, inauthor: String?, inpublisher: String?) {
-        val url = "https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC"
+    private fun fetchBooks(url:String) {
         val client = OkHttpClient()
         val req = Request.Builder().url(url).build()
         client.newCall(req).enqueue(object: Callback {  // cannot use .execute() in the UI thread
             override fun onResponse(call: Call?, response: Response?) {
                 val body = response?.body()?.string()
-                // println(body)
                 println("Works like a charm!")
+                println(body)
             }
             override fun onFailure(call: Call?, e: IOException?) {
                 println("Epic fail!")
             }
         })
+    }
+
+    private fun advancedSearch(isbn: String?, intitle: String?, inauthor: String?, inpublisher: String?) {
+        //val apiKey = "AIzaSyDWQIKMYe6760oFGDbNli8lBVt6IYxga7g"
+        val url = "https://www.googleapis.com/books/v1/volumes/?q=" +
+                "isbn:$isbn" +
+                "+intitle:$intitle" +
+                "+inauthor:$inauthor" +
+                "+inpublisher:$inpublisher"
+        print(url)
+        fetchBooks(url)
     }
 }
