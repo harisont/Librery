1c75535404f1408f09dc2b0ca85e97141410a1b2
Arianna Masciolini <uzkamascio@gmail.com>
2018-06-11 18:16:41 +0200

    Close #10, close #16.

--- LibreryApp/app/src/main/java/com/example/harisont/librery/SearchActivity.kt
+++ LibreryApp/app/src/main/java/com/example/harisont/librery/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_search.*
 import okhttp3.*
 import java.io.IOException
@@ -35,6 +36,8 @@ class SearchActivity : AppCompatActivity() {
             }
             override fun onFailure(call: Call?, e: IOException?) {
                 println("Epic fail!")
+                runOnUiThread {
+                    Toast.makeText(this@SearchActivity, getString(R.string.query_failure), Toast.LENGTH_LONG).show() }
             }
         })
     }
@@ -45,14 +48,14 @@ class SearchActivity : AppCompatActivity() {
         val inauthor = if (author != "") "inauthor:$author" else ""
         val inpublisher = if (publisher != "") "inpublisher:$publisher" else ""
         val searchParameters = "q=$isbn+$intitle+$inauthor+$inpublisher"
-        if (searchParameters == "q=+++") {  //if there are no search parameters
+        if (searchParameters == "q=+++") {  // if there are no search parameters (check could be improved)
             Toast.makeText(this, getString(R.string.fill_fields_toast), Toast.LENGTH_LONG).show()
-
         }
         else {
             val url = "https://www.googleapis.com/books/v1/volumes/?" +
                     "$searchParameters" +
                     "&projection=lite"
+            println("URL: $url")
             fetchBooks(url)
         }
     }
