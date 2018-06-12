1ae28318c55fbfbc8b16855bc6da175ba0dc4bf0
Arianna Masciolini <uzkamascio@gmail.com>
2018-06-12 18:03:09 +0200

    #18 DAO.

--- LibreryApp/app/src/main/java/com/example/harisont/librery/BookDAO.kt
+++ LibreryApp/app/src/main/java/com/example/harisont/librery/BookDAO.kt
@@ -2,14 +2,26 @@ package com.example.harisont.librery
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 
 @Dao
 interface BookDAO {
 
-    @Insert
+    @Query("SELECT * FROM BookEntity")
+    fun selectAll(): List<BookEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun saveBookData(book: BookEntity)
 
-    @Query("SELECT * FROM BookEntity")
-    fun selectAll() : List<BookEntity>
+    @Query("SELECT title, authors, thumbnailURL FROM BookEntity WHERE read = 1")
+    fun selectHaveRead(): List<BookEntity>
+
+    @Query("SELECT title, authors, thumbnailURL FROM BookEntity WHERE read = 0")
+    fun selectToRead(): List<BookEntity>
+
+    @Query("SELECT * from BookEntity WHERE id = :id")
+    fun selectBook(id: String)
+
 }
+
