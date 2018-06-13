4f296d4201e30f3c9a70ad9fbca19d18d48c09a8
Arianna Masciolini <uzkamascio@gmail.com>
2018-06-13 22:14:47 +0200

    Thread for API call.

--- LibreryApp/app/src/main/java/com/example/harisont/librery/SearchActivity.kt
+++ LibreryApp/app/src/main/java/com/example/harisont/librery/SearchActivity.kt
@@ -8,6 +8,7 @@ import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_search.*
 import okhttp3.*
 import java.io.IOException
+import kotlin.concurrent.thread
 
 class SearchActivity : AppCompatActivity() {
 
@@ -27,19 +28,23 @@ class SearchActivity : AppCompatActivity() {
     private fun fetchBooks(url:String) {
         val client = OkHttpClient()
         val req = Request.Builder().url(url).build()
-        client.newCall(req).enqueue(object: Callback {  // cannot use .execute() in the UI thread
-            override fun onResponse(call: Call?, response: Response?) {
-                val json = response?.body()?.string()
-                println("Works like a charm!")
-                startActivity(Intent(this@SearchActivity, SearchResultsActivity::class.java)
-                        .putExtra("res", json))     // search results are sent to the new activity as JSON
-            }
-            override fun onFailure(call: Call?, e: IOException?) {
-                println("Epic fail!")
-                runOnUiThread {
-                    Toast.makeText(this@SearchActivity, getString(R.string.query_failure), Toast.LENGTH_LONG).show() }
-            }
-        })
+        thread {
+            client.newCall(req).enqueue(object : Callback {  // cannot use .execute() in the UI thread
+                override fun onResponse(call: Call?, response: Response?) {
+                    val json = response?.body()?.string()
+                    println("Works like a charm!")
+                    startActivity(Intent(this@SearchActivity, SearchResultsActivity::class.java)
+                            .putExtra("res", json))     // search results are sent to the new activity as JSON
+                }
+
+                override fun onFailure(call: Call?, e: IOException?) {
+                    println("Epic fail!")
+                    runOnUiThread {
+                        Toast.makeText(this@SearchActivity, getString(R.string.query_failure), Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+        }
     }
 
     private fun advancedSearch(isbnCode: String, title: String, author: String, publisher: String) {
