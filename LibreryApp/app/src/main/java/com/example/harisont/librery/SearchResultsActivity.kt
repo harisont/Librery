5b84eb0f7d0194fe935671d1160287b57abbb19d
Arianna Masciolini <uzkamascio@gmail.com>
2018-06-14 21:56:19 +0200

    Close #10 for last time!

--- LibreryApp/app/src/main/java/com/example/harisont/librery/SearchResultsActivity.kt
+++ LibreryApp/app/src/main/java/com/example/harisont/librery/SearchResultsActivity.kt
@@ -17,10 +17,11 @@ class SearchResultsActivity : AppCompatActivity() {
         val gson = GsonBuilder().create()
         val searchResults = gson.fromJson(json, SearchResults::class.java)
         recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.adapter = RecyclerViewAdapter(searchResults.items)
-        if (recycler_view.adapter.itemCount == 0) {
+        if (searchResults.items == null) {
             Toast.makeText(this@SearchResultsActivity, R.string.no_matching_data, Toast.LENGTH_LONG).show()
             finish()
         }
+        else
+            recycler_view.adapter = RecyclerViewAdapter(searchResults.items)
     }
 }
