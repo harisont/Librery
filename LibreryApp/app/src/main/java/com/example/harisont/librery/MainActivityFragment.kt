b3e4e9016979615c422f0d391318a0c6655cbef2
Arianna Masciolini <uzkamascio@gmail.com>
2018-06-14 12:49:25 +0200

    Fragment.

---
+++ LibreryApp/app/src/main/java/com/example/harisont/librery/MainActivityFragment.kt
@@ -0,0 +1,14 @@
+package com.example.harisont.librery
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainActivityFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // TODO: add layout
+        return View(activity)
+    }
+}
